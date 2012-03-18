2c0564a828f36779bbc5677c923eef859b83eeea
Jonathan Chung <jono.chung@gmail.com>
2012-03-18 15:31:12 -0700

    Added the 5 tabs and a class for the news feed.

---
+++ ECE_Reference/NewsFeed.m
@@ -0,0 +1,60 @@
+//
+//  UpdateViewController.m
+//  ECE_Reference
+//
+//  Created by 薇葭 张 on 12-03-18.
+//  Copyright (c) 2012 UBC. All rights reserved.
+//
+
+#import "NewsFeed.h"
+
+@implementation UpdateViewController
+@synthesize UpdateLabel;
+@synthesize updateButton;
+
+-(IBAction)buttonPressed:(id)sender
+{
+    UpdateLabel.text=@"HOHOHO";
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [self setUpdateLabel:nil];
+    [self setUpdateButton:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
